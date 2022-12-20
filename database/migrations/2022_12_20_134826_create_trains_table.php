e4e5da3eb4aa760d841b18b28f90bc8aeb40fbc5
Andrearusso94 <andrea.r.4845@gmail.com>
2022-12-20 14:59:53 +0100

    add component table trains

--- database/migrations/2022_12_20_134826_create_trains_table.php
+++ database/migrations/2022_12_20_134826_create_trains_table.php
@@ -15,7 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('Azienda', 15);
+            $table->string('Stazione di partenza', 100);
+            $table->string('Stazione di arrivo', 100);
+            $table->float('Orario di partenza', 24, 2);
+            $table->string('Orario di arrivo', 24, 2);
+            $table->float('Codice Treno', 100);
+            $table->float('Numero Carrozze', 9);
+            $table->string('In orario', 50);
+            $table->string('Cancellato', 50);
         });
     }
 
